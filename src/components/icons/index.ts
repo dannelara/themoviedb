be3418271a8415d54c993fe51a1b62e0c4880c19
Daniel <11daniel@live.se>
2023-01-24 23:59:43 +0100

    .

---
+++ src/components/icons/index.ts
@@ -0,0 +1,3 @@
+import { HomeIcon } from "./homeIcon/HomeIcon";
+import { DiscoveryIcon } from "./discoveryIcon/DiscoveryIcon";
+export { HomeIcon, DiscoveryIcon };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import { Main } from "./main/Main";
 import { Nav } from "./nav/Nav";
 import { SearchBar } from "./searchbar/SearchBar";
-// import { FullMovieView } from "./fullMovieView/FullMovieView";
+
 export { Main, Nav, SearchBar };
