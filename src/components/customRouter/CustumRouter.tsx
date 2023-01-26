216f3e85504a8d15dd728071f6e649cc9b859daa
Daniel <11daniel@live.se>
2023-01-26 13:59:56 +0100

    restructured folders

--- src/components/customRouter/CustumRouter.tsx
+++ src/components/customRouter/CustumRouter.tsx
@@ -1,13 +1,13 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Discover } from "components/discover/Discover";
+import { Discover } from "components/views/discover/Discover";
 import { Loader } from "utils/loader/Loader";
-import { SearchView } from "components/searchView/SearchView";
+import { SearchView } from "components/views/search/SearchView";
 
 // Lazy load these two components since they're the ones loading data.
-const Home = React.lazy(() => import("components/home/Home"));
+const Home = React.lazy(() => import("components/views/home/Home"));
 const FullMovieView = React.lazy(
-  () => import("components/fullMovieView/FullMovieView")
+  () => import("components/views/fullMovie/FullMovieView")
 );
 
 export const CustumRouter: React.FC = ({}) => {
